2e86799103e21433357aa3351001b4fb871923ab
Wizout <ozziewei@gmail.com>
2021-02-21 17:04:17 +0800

    x platform #include

--- src/net/common.h
+++ src/net/common.h
@@ -3,6 +3,9 @@
 
 
 #pragma once
+#ifdef _WIN32
+#include    <WinSock2.h>
+#elif __linux__
 #include	<arpa/inet.h>	      /* inet(3) functions */
 #include	<netinet/in.h>	    /* sockaddr_in{} and other Internet defns */
 #include	<sys/socket.h>	    /* basic socket definitions */
@@ -15,9 +18,9 @@
 #include	<errno.h>
 #include	<fcntl.h>		        /* for nonblocking */
 #include	<netdb.h>
-#include	<signal.h>
 #include	<unistd.h>
-#include  <cstdint>
+#endif
+#include    <cstdint>
 #include	<cstdio>
 #include	<cstdlib>           /* perror(), etc...  */
 #include	<cstring>
